dd4acaba1e732ae8957386029ab11a22ae921de9
brainmorph <capota.d@gmail.com>
2019-09-22 18:47:27 -0500

    test to see if able to run at 160MHz (yes!).  Add 4 PWM channels for TIM4

--- Src/main.c
+++ Src/main.c
@@ -20,12 +20,10 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "circularBuffer.h"
-#include "stdio.h"
-//#include "stm32f4xx_hal.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "circularbuffer.h"
 
 /* USER CODE END Includes */
 
@@ -52,6 +50,7 @@ TIM_HandleTypeDef htim6;
 TIM_HandleTypeDef htim7;
 
 UART_HandleTypeDef huart4;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -224,6 +223,28 @@ float pwm(float value)
 }
 
 uint32_t NOW_MS = 0;
+
+
+//TODO: CLEAN UPVVVVVVVVVVVVVVVVVVVVVVVVVVV
+#define start_timer()    *((volatile uint32_t*)0xE0001000) = 0x40000001  // Enable CYCCNT register
+#define stop_timer()   *((volatile uint32_t*)0xE0001000) = 0x40000000  // Disable CYCCNT register
+#define get_timer()   *((volatile uint32_t*)0xE0001004)               // Get value from CYCCNT register
+uint32_t it1, it2;      // start and stop flag
+//stop_timer();               // If timer is not needed any more, stop
+
+void user_pwm_setvalue(uint32_t channel, uint16_t value)
+{
+    TIM_OC_InitTypeDef sConfigOC;
+
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    sConfigOC.Pulse = value;
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+//    HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1);
+    HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, channel);
+    HAL_TIM_PWM_Start(&htim4, channel);
+}
+//TODO: CLEAN UP^^^^^^^^^^^^^^^
 /* USER CODE END 0 */
 
 /**
@@ -235,6 +256,7 @@ int main(void)
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
+  
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -302,43 +324,97 @@ int main(void)
   envAccelY = avgAccelY;
   envAccelZ = avgAccelZ;
 
+  //TODO: CLEAN UPVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+	start_timer();          // start the timer.
+	it1 = get_timer();      // store current cycle-count in a local
+
+	// do something
+
+	it2 = get_timer() - it1;    // Derive the cycle-count difference
+	it1 = it2;
+
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);
+
+	int counter = 0;
+	int counter2 = 0;
+
+void dumbFunction(void)
+{
+	counter++;
+	if(counter < 0)
+	  counter = 0;
+	if(counter2 < 0)
+	  counter2 = 0;
+
+	it2 = get_timer();    // Derive the cycle-count difference
+	it1 = it2 - it1;
+
+	//HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_3);
+
+	//	  user_pwm_setvalue(TIM_CHANNEL_1, 500 + counter);
+	//	  user_pwm_setvalue(TIM_CHANNEL_2, 5000 + counter);
+	//	  user_pwm_setvalue(TIM_CHANNEL_3, 50000 + counter);
+	//	  user_pwm_setvalue(TIM_CHANNEL_4, counter);
+
+//	if(counter % 10 != 0)
+//	  return;
+
+	counter2++;
+	__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, counter2);
+	__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_2, 5000 + counter2);
+	__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 50000 + counter2);
+	__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_4, counter2);
+
+	it1 = it2;
+}
+	//TODO: CLEAN UP ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   while (1)
   {
+	  //it2 = get_timer();    // Derive the cycle-count difference
+	  //it1 = it2 - it1;
 
-	  // read acceleration, filter with a running average
-	  readCurrentAccelerationValues(&avgAccelX, &avgAccelY, &avgAccelZ);
-	  accelRunningAverage(&avgAccelX, &avgAccelY, &avgAccelZ); // takes input and factors it into the running average for each variable
+	  //dumbFunction();
+	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_3);
 
-	  // at this point I should have the running average of floatX,Y,Z according to accelRunningAverage(...)
+	  //it1 = it2;
 
-	  // for fun let's deadband accelZ.
-	  float deadBandZ = (avgAccelZ - envAccelZ);
-	  if(deadBandZ < 0.2 && deadBandZ > -0.2)
-		  deadBandZ = 0;
-
-	  // derive velocity from acceleration
-	  vX = (avgAccelX - envAccelX) * deltaT + vX; // subtract out calibration
-	  vY = (avgAccelY - envAccelY) * deltaT + vY; // subtract out calibration
-	  vZ = deadBandZ * deltaT + vZ; // subtract out calibration
-
-	  // calculate error
-	  float errorVZ = 0.0 - vZ; // my setpoint is 0 m/s for now.  + vZ because positive Z axis points down
-
-	  // PID (P only for now)
-	  float thrustRPM = 20000.0 * errorVZ;
-
-	  // convert to PWM
-	  int PWM = (int)pwm(thrustRPM);
-
-	  if(PWM < 0)
-		  PWM = 0;
-	  if(PWM > 255)
-		  PWM = 255;
-
-	  // send PWM to UART
-	  uint8_t uartData[70];
-	  snprintf(uartData, sizeof(uartData), "<%ld, %f.2,%3d, %3d, %3d, %3d, %d>", count, vZ, PWM, PWM, PWM, PWM, NOW_MS);
-	  HAL_UART_Transmit(&huart4, uartData, 70, 0x00FF);  //TODO: choose the correct number of bytes to send
+//	  // read acceleration, filter with a running average
+//	  readCurrentAccelerationValues(&avgAccelX, &avgAccelY, &avgAccelZ);
+//	  accelRunningAverage(&avgAccelX, &avgAccelY, &avgAccelZ); // takes input and factors it into the running average for each variable
+//
+//	  // at this point I should have the running average of floatX,Y,Z according to accelRunningAverage(...)
+//
+//	  // for fun let's deadband accelZ.
+//	  float deadBandZ = (avgAccelZ - envAccelZ);
+//	  if(deadBandZ < 0.2 && deadBandZ > -0.2)
+//		  deadBandZ = 0;
+//
+//	  // derive velocity from acceleration
+//	  vX = (avgAccelX - envAccelX) * deltaT + vX; // subtract out calibration
+//	  vY = (avgAccelY - envAccelY) * deltaT + vY; // subtract out calibration
+//	  vZ = deadBandZ * deltaT + vZ; // subtract out calibration
+//
+//	  // calculate error
+//	  float errorVZ = 0.0 - vZ; // my setpoint is 0 m/s for now.  + vZ because positive Z axis points down
+//
+//	  // PID (P only for now)
+//	  float thrustRPM = 20000.0 * errorVZ;
+//
+//	  // convert to PWM
+//	  int PWM = (int)pwm(thrustRPM);
+//
+//	  if(PWM < 0)
+//		  PWM = 0;
+//	  if(PWM > 255)
+//		  PWM = 255;
+//
+//	  // send PWM to UART
+//	  uint8_t uartData[70];
+//	  snprintf(uartData, sizeof(uartData), "<%ld, %f.2,%3d, %3d, %3d, %3d, %d>", count, vZ, PWM, PWM, PWM, PWM, NOW_MS);
+//	  HAL_UART_Transmit(&huart4, uartData, 70, 0x00FF);  //TODO: choose the correct number of bytes to send
 
 	  // print stuff out (a.k.a send to UART)
 //	  uint8_t uartData[150];
@@ -375,7 +451,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 10;
+  RCC_OscInitStruct.PLL.PLLN = 200;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -384,12 +465,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
@@ -450,7 +531,7 @@ static void MX_TIM4_Init(void)
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 0;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 0;
+  htim4.Init.Period = 60000;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
@@ -464,21 +545,24 @@ static void MX_TIM4_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
+  sConfigOC.Pulse = 500;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
   {
     Error_Handler();
   }
+  sConfigOC.Pulse = 5000;
   if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
   }
+  sConfigOC.Pulse = 50000;
   if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
     Error_Handler();
   }
+  sConfigOC.Pulse = 0;
   if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
   {
     Error_Handler();
@@ -508,10 +592,9 @@ static void MX_TIM6_Init(void)
 
   /* USER CODE END TIM6_Init 1 */
   htim6.Instance = TIM6;
-  //htim6.Init.Prescaler = 0;
-  htim6.Init.Prescaler = 159;
+  htim6.Init.Prescaler = 0;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 99;
+  htim6.Init.Period = 0;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
@@ -607,11 +690,22 @@ static void MX_UART4_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PD3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 }
 
