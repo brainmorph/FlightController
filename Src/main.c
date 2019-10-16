18dd1ee691fd6068060d6969628dbdb7615c29db
brainmorph <capota.d@gmail.com>
2019-10-16 15:16:27 -0500

    change pitch and yaw sensitivity to control input

--- Src/main.c
+++ Src/main.c
@@ -606,9 +606,9 @@ int main(void)
 	void mixPWM(float thrust, float roll, float pitch, float yaw)
 	{
 		// TODO: move these multipliers into 3 separate PID loops, one for each control axis
-		//pitch *= 2.5; // make pitch a little bit stronger than roll since the battery packs lie on this axis
+		pitch *= 2.0; // make pitch a little bit stronger than roll since the battery packs lie on this axis
 		//roll *= 1.0;
-		yaw /= 4.0; // yaw needs to be cut back heavily
+		yaw /= 8.0; // yaw needs to be cut back heavily
 
 		FR = thrust - yaw + pitch - roll;
 		FL = thrust + yaw + pitch + roll;
@@ -716,15 +716,6 @@ int main(void)
 
 
 		// calculate derivative
-//		if(lpfErrorRollOLD < errorRoll) // apply lpf directionally otherwise you get unbounded DC term
-//			lpfErrorRoll += 0.01 * fabs(errorRoll); // lpf the error signal to prepare for derivative
-//		else
-//			lpfErrorRoll -= 0.01 * fabs(errorRoll);
-//
-//		if(lpfErrorPitchOLD < errorPitch)
-//			lpfErrorPitch += 0.01 * fabs(errorPitch); // lpf the error signal to prepare for derivative
-//		else
-//			lpfErrorPitch -= 0.01 * fabs(errorPitch);
 		lpfErrorRoll = lpfErrorRoll + 0.1 * (errorRoll - lpfErrorRoll);
 		lpfErrorPitch = lpfErrorPitch + 0.1 * (errorPitch - lpfErrorPitch);
 
