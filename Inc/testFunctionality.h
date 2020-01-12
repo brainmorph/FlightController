e688977a0ab8d5f7a0856c596441b200a2dfd27d
brainmorph <capota.d@gmail.com>
2020-01-11 19:18:57 -0600

    Begin interface to "testFunctionality" module

---
+++ Inc/testFunctionality.h
@@ -0,0 +1,14 @@
+/*
+ * testFunctionality.h
+ *
+ *  Created on: Jan 11, 2020
+ *      Author: DC
+ */
+
+#ifndef TESTFUNCTIONALITY_H_
+#define TESTFUNCTIONALITY_H_
+
+void testI2C_BME();
+void spiNRF24();
+
+#endif /* TESTFUNCTIONALITY_H_ */
